58cd643545e7dda989f99f6b9a536081642e0171
Sheryn Ait-tabet <sheryn@Mac.lan>
2026-06-12 01:33:30 +0200

    auto-deploy

--- src/pages/Etiquettes.jsx
+++ src/pages/Etiquettes.jsx
@@ -79,7 +79,7 @@ export default function Etiquettes() {
 
   const labelOrders = sortedSelectedDays
     .flatMap((day, dayIdx) => ordersForDay(day).map(o => ({ order: o, colorIndex: dayIdx })))
-  const labelCount = labelCount
+  const labelCount = labelOrders.length
 
   function handlePrint() {
     window.print()
